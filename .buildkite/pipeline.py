dee80fac56664684fcca89959e613fe8a5303730
Christian Nunciato <chris@nunciato.org>
2025-08-26 14:16:28 -0700

    Ubuntu 24

--- .buildkite/pipeline.py
+++ .buildkite/pipeline.py
@@ -3,8 +3,10 @@ from buildkite_sdk import Pipeline
 pipeline = Pipeline()
 pipeline.add_step({
     'commands': [
+        'brew install uv',
         'uv run main.py'
-    ]
+    ],
+    'image': 'ubuntu:24.04'
 })
 
 print(pipeline.to_json())
