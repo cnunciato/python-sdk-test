ae3ac1c14aabeace84b3af266b0088e3cab3a34b
Christian Nunciato <chris@nunciato.org>
2025-08-26 14:38:37 -0700

    Try Linux

--- .buildkite/pipeline.py
+++ .buildkite/pipeline.py
@@ -7,6 +7,7 @@ pipeline.add_step({
     },
     'commands': [
         'curl -LsSf https://astral.sh/uv/install.sh | sh',
+        'export PATH=$$HOME/.local/bin:$PATH',
         'uv run main.py'
     ],
     'image': 'ubuntu:24.04'
